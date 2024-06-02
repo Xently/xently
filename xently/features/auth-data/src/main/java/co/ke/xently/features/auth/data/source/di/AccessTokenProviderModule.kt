6233fa6899da92d3f814bffeb955c3222d54a10d
Orinda Harrison (Mitch) <mitchke@duck.com>
2024-06-02 17:37:28 +0300

    Extract repository to interface

---
+++ xently/features/auth-data/src/main/java/co/ke/xently/features/auth/data/source/di/AccessTokenProviderModule.kt
@@ -0,0 +1,19 @@
+package co.ke.xently.features.auth.data.source.di
+
+import co.ke.xently.features.auth.data.source.AuthenticationDatabase
+import co.ke.xently.libraries.data.network.AccessTokenProvider
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AccessTokenProviderModule {
+    @Provides
+    @Singleton
+    fun provideAccessTokenProvider(database: AuthenticationDatabase): AccessTokenProvider {
+        return AccessTokenProvider { database.userDao().first()?.accessToken }
+    }
+}
--- xently/features/auth-data/src/main/java/co/ke/xently/features/auth/data/source/di/AuthenticationModule.kt
+++ xently/features/auth-data/src/main/java/co/ke/xently/features/auth/data/source/di/AuthenticationModule.kt
@@ -1,19 +1,15 @@
 package co.ke.xently.features.auth.data.source.di
 
-import co.ke.xently.features.auth.data.source.AuthenticationDatabase
-import co.ke.xently.libraries.data.network.AccessTokenProvider
+import co.ke.xently.features.auth.data.source.UserRepository
+import co.ke.xently.features.auth.data.source.UserRepositoryImpl
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AuthenticationModule {
-    @Provides
-    @Singleton
-    fun provideAccessTokenProvider(database: AuthenticationDatabase): AccessTokenProvider {
-        return AccessTokenProvider { database.userDao().first()?.accessToken }
-    }
+internal abstract class AuthenticationModule {
+    @Binds
+    abstract fun bindsUserRepository(repository: UserRepositoryImpl): UserRepository
 }
