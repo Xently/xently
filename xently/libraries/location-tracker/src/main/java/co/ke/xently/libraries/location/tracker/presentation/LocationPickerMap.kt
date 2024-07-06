6167f1cb117cf66b60703984e1971a7e05edeb85
Orinda Harrison (Mitch) <mitchke@duck.com>
2024-07-06 19:47:06 +0300

    Disable draggable map marker

--- xently/libraries/location-tracker/src/main/java/co/ke/xently/libraries/location/tracker/presentation/LocationPickerMap.kt
+++ xently/libraries/location-tracker/src/main/java/co/ke/xently/libraries/location/tracker/presentation/LocationPickerMap.kt
@@ -5,10 +5,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -17,7 +14,6 @@ import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.PointOfInterest
 import com.google.maps.android.compose.CameraPositionState
-import com.google.maps.android.compose.DragState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapUiSettings
@@ -43,36 +39,6 @@ inline fun LocationPickerMap(
             LatLng(location.latitude, location.longitude).let {
                 markerState.position = it
                 cameraPositionState.position = CameraPosition.fromLatLngZoom(it, 18f)
-            }
-        }
-    }
-
-    var wasMarkerDragStarted by remember {
-        mutableStateOf(false)
-    }
-
-    LaunchedEffect(markerState.dragState) {
-        when (markerState.dragState) {
-            DragState.END -> {
-                if (wasMarkerDragStarted) {
-                    // DragState.END is called by default, but we only want to
-                    // update the location state if the drag was actually
-                    // triggered. A triggered drag must first have the state
-                    // as START.
-                    Location(
-                        latitude = markerState.position.latitude,
-                        longitude = markerState.position.longitude,
-                    ).let(onMarkerPositionChange)
-
-                    wasMarkerDragStarted = false
-                }
-            }
-
-            DragState.START -> {
-                wasMarkerDragStarted = true
-            }
-
-            DragState.DRAG -> {
             }
         }
     }
@@ -112,7 +78,6 @@ inline fun LocationPickerMap(
         ) {
             Marker(
                 state = markerState,
-                draggable = true,
                 visible = location != null,
                 onClick = {
                     Location(
